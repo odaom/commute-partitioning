c44cabcff50faf3b1c05c2a41d84f11c99094355
Oda Omholt <oda.omholt@unacast.com>
2019-07-23 15:10:41 +0200

    Convert to python3 and generalize data source

--- combo-postprocessor.py
+++ combo-postprocessor.py
@@ -3,33 +3,33 @@ Commutes to Combo Postprocessor
 Garrett Dash Nelson and Alasdair Rae, 2016
 
 Read the paper: http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0166083
+
+Generalized for arbitrary commute data sources.
 '''
 
 from os import path
 
 # Check if Combo results are available
 if not path.isfile('data-stage1/commutes_comm_comboC++.txt'):
-	print 'Failed: Could not find Combo output file at ./data-stage1/commutes_comm_comboC++.txt'
+	print('Failed: Could not find Combo output file at ./data-stage1/commutes_comm_comboC++.txt')
 	exit()
 
 if not path.isdir('data-final'):
-	print 'Failed: Requires an output directory data-final'
+	print('Failed: Requires an output directory data-final')
 	exit()
 
-comboRawOutput = open('data-stage1/commutes_comm_comboC++.txt')
-communities = comboRawOutput.readlines()
+with open('data-stage1/commutes_comm_comboC++.txt') as comboRawOutput:
+	communities = comboRawOutput.readlines()
 
-outFile = open('data-final/fips_table_with_community_assignments.csv','w')
+with open('data-final/area_code_table_with_community_assignments.csv','w') as outFile:
 
-fipsRawInput = open('data-stage1/fips_table.csv')
+	with open('data-stage1/area_code_table.csv') as areaCodeRawInput:
 
+		next(areaCodeRawInput) # skip header
 
+		outFile.write('serial_id,area_code,community\n')
 
-fipsRawInput.next() # skip header
-
-outFile.write('serial_id,fips,community\n')
-
-i = 0
-for line in fipsRawInput:
-	outFile.write(line.rstrip() + ',' + str(communities[i]))
-	i = i+1
+		i = 0
+		for line in areaCodeRawInput:
+			outFile.write(line.rstrip() + ',' + str(communities[i]))
+			i += 1
